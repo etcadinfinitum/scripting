e2e44d89dd6314c187aeea77b52084ed66eff5b3
etcadinfinitum <lizzy.presland@gmail.com>
2019-10-27 02:25:30 -0700

    Polish script for results comparison
    This will allow massive HTML files to be removed from the directory.

--- media_server/output/compare.sh
+++ media_server/output/compare.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-cat $1 | sed -e "s/^$//g; s/^\(\t\)*\(.*\)/\2/g" > 1.html
-cat expected.html | sed -e "s/^$//g; s/^\( \)*\(.*\)/\2/g" > expected_test.html
-vimdiff 1.html expected_test.html
+if [[ -z $2 || ! -d ../$2 ]]; then
+    echo "Fatal: invalid directory for ARG2"
+    exit 1
+fi
+if [[ -z $1 || ! -x ../$1 ]]; then
+    echo "Fatal: invalid script file for ARG1"
+    exit 1
+fi
+cd .. && find $2 | bash $1 > output/result.html
+cd output/
+curl https://gist.githubusercontent.com/etcadinfinitum/962ee7ad3250122e36fb54572d399cac/raw/1127fb535bfa56fd3472824c818e33696a7b49f1/expected.html > expected.html
+sed -e "s/^$//g; s/^\(\t\)*\(.*\)/\2/g" -i result.html
+sed -e "s/^$//g; s/^\( \)*\(.*\)/\2/g" -i expected.html
+vimdiff result.html expected.html
+rm result.html expected.html
